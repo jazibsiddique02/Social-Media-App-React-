46b3f5121672f759d8c5afbcd7bbe1cce5d28d73
jazibsiddique02 <rajajazibsiddique@gmail.com>
2024-11-16 19:38:36 +0500

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Sidebar from "./Components/Sidebar";
+import CreatePost from "./Components/CreatePost";
+import PostList from "./Components/PostList";
 
 function App() {
   return (
-    <>
-      <Header />
-    </>
+    <div className="app-container">
+      <Sidebar />
+      <div className="content">
+        <Header />
+        <CreatePost />
+        <PostList />
+        <Footer />
+      </div>
+    </div>
   );
 }
 
